f0e0355c447052a8af765cbda75d3ce176fe31df
joheee <johevinblesstowi07@gmail.com>
2025-08-27 13:52:30 +0700

    [0217] time 51ms (58.02%), memory 90.78mb (61.12%)

--- 0217-contains-duplicate/main.cpp
+++ 0217-contains-duplicate/main.cpp
@@ -5,7 +5,7 @@ public:
         unordered_map<int,int> umap;
         // iterate with o(n) time complexity
         for(int i = 0; i < nums.size(); i++) {
-            // if the curr already exist inside umap (it means duplicate has been found)
+            // if the curr already exist inside umap, it means duplicate has been found
             if(umap.count(nums[i])) return true;
             // save the curr into umap
             umap[nums[i]] = 0;
