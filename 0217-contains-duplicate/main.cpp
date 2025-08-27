1477fc613687968c75ec1bdd359ec4cde3ff93a6
joheee <johevinblesstowi07@gmail.com>
2025-08-27 13:52:03 +0700

    [0217] time 51ms (58.02%), memory 90.78mb (61.12%)

---
+++ 0217-contains-duplicate/main.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool containsDuplicate(vector<int>& nums) {
+        // storing umap with format {nums[i]:i}
+        unordered_map<int,int> umap;
+        // iterate with o(n) time complexity
+        for(int i = 0; i < nums.size(); i++) {
+            // if the curr already exist inside umap (it means duplicate has been found)
+            if(umap.count(nums[i])) return true;
+            // save the curr into umap
+            umap[nums[i]] = 0;
+        }
+        return false;   
+    }
+};
